53778c15c8a1341c747df0fd3c98b51b99dc706f
Pedro Castro <pcastro9876@gmail.com>
2025-05-22 14:20:02 -0300

    criando service de cadastro no banco de dados

--- src/services/users/CreateUserService.ts
+++ src/services/users/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma/index";
+
 interface UserRequest {
   name: string;
   email: string;
@@ -6,7 +8,38 @@ interface UserRequest {
 
 class CreateUserService {
   static async CriarUsuario({name,email,password}: UserRequest){
-    return {name: name, email: email, password: password};// Simulação de criação de usuário
+    
+    // Verificar o email enviado
+    if(!email){
+      throw new Error('Email é obrigatório');
+    }
+    // Verificar se o email já existe
+    const userExists = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if(userExists){
+      throw new Error('Email já cadastrado');
+    }
+
+    // Cadastrar o usuário no banco
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    })
+
+
+    return user;// Simulação de criação de usuário
   }
 }
 
